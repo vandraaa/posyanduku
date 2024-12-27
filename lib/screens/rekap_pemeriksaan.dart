8de1af265ac537cc7508b7dcd24580566cea4a95
Annsfza <kutubsangatdingin@gmail.com>
2024-12-27 15:06:59 +0800

    edukasi dan rekap pemeriksaan

---
+++ lib/screens/rekap_pemeriksaan.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class RekapPemeriksaanScreen extends StatelessWidget {
+  const RekapPemeriksaanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.lightBlue.shade300,
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            color: Colors.lightBlue.shade300,
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'Halo Kevin',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Center(
+                  child: Text(
+                    'Puskesmas Purwosari, Surakarta',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Container(
+                    height: 120,
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 1),
+                      borderRadius: BorderRadius.circular(8),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/rekap/1.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        _buildImageItem('assets/images/rekap/2.png'),
+                        const SizedBox(height: 16),
+                        _buildImageItem('assets/images/rekap/3.png'),
+                        const SizedBox(height: 16),
+                        _buildImageItem('assets/images/rekap/4.png'),
+                        const SizedBox(height: 16),
+                        _buildImageItem('assets/images/rekap/5.png'),
+                        const SizedBox(height: 16),
+                        _buildImageItem('assets/images/rekap/6.png'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImageItem(String imagePath) {
+    return Container(
+      height: 120,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white, width: 1),
+        borderRadius: BorderRadius.circular(8),
+        image: DecorationImage(
+          image: AssetImage(imagePath),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
